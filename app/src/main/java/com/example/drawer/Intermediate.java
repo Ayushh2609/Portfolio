e17e4fdaa5cb6841038e07294c053c633115c274
Ayushh0708 <paliwalaayush717@gmail.com>
2025-07-01 21:28:39 +0530

    Some changes

--- app/src/main/java/com/example/drawer/Intermediate.java
+++ app/src/main/java/com/example/drawer/Intermediate.java
@@ -23,7 +23,6 @@ public class Intermediate extends Fragment {
     }
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
