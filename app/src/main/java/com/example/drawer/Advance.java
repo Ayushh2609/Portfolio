0207949979bd20f73ad477731cbe9f76d9ad3c57
Ayushh0708 <paliwalaayush717@gmail.com>
2025-07-03 21:04:14 +0530

    Some changes

--- app/src/main/java/com/example/drawer/Advance.java
+++ app/src/main/java/com/example/drawer/Advance.java
@@ -53,7 +53,6 @@ public class Advance extends Fragment {
         arrayListNukkadEatsAdmin.add(R.drawable.nukkad_admin6);
 
 
-
         RecyclerView recyclerviewAdvance = view.findViewById(R.id.recyclerAdvance);
         recyclerviewAdvance.setLayoutManager(new LinearLayoutManager(getContext()));
 
